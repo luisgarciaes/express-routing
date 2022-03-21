6eedd136ecb54905e2fb700145242475100e94c1
ruruisu <zaddozgl@gmail.com>
2022-03-20 20:26:20 -0600

    second route

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
-app.get('/', (req, res =>{
+app.get('/', (req, res) =>{
     res.send('<html><head></head><body><h1>Hola mundito</h1></body></html>')
-}))
+});
+app.get('/api', (req, res) =>{
+    res.json({ firstname: 'Luis', lastname: 'Garcia'})
+});
 
 app.listen(port);
